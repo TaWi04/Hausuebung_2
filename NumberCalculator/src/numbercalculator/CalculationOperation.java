e507219ee42550edcfe7eee06dcd67168c8a642a
unknown <tamarawimmer04@gmail.com>
2020-10-05 12:08:44 +0200

    created class "AbstractCalculator" and interface "CalculationOperation"

--- NumberCalculator/src/numbercalculator/CalculationOperation.java
+++ NumberCalculator/src/numbercalculator/CalculationOperation.java
@@ -9,13 +9,6 @@ package numbercalculator;
  *
  * @author Tamara
  */
-public class NumberCalculator {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public interface CalculationOperation {
+    Number calc(Number x, Number y);
 }
